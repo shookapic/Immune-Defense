d109b60e9fa02d072275bf28b41dfef553987f78
Frederik L. Andersen <frederiklind@hotmail.dk>
2025-10-07 07:53:29 +0200

    Add: NGO, support packages\nRemoved: ParrelSync

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,105 +1,78 @@
 using Unity.Netcode;
-using Unity.Netcode.Components;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(NetworkTransform))]
 public class PlayerMovement : NetworkBehaviour
 {
-    private Rigidbody2D rb;
-
-    [SerializeField] private float movementSpeed = 10f;
-    [SerializeField] private float rotationSmoothness = 10f;
-    [SerializeField] private float movementSmoothness = 10f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float moveSmooth = 10f;
+    [SerializeField] private float rotSmooth = 10f;
     [SerializeField] private float bounceForce = 10f;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
-    private Vector2 _moveInput;
-    private bool _fire;
-
-    [SerializeField] private SpriteRenderer spriteRenderer; // Reference to flip visuals
+    private Rigidbody2D rb;
+    private Vector2 input;
+    private bool fire;
 
     public override void OnNetworkSpawn()
     {
         rb = GetComponent<Rigidbody2D>();
-
         rb.bodyType = IsServer ? RigidbodyType2D.Dynamic : RigidbodyType2D.Kinematic;
-
-        enabled = IsOwner;
 
         if (!spriteRenderer)
             spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        enabled = IsOwner;
     }
 
-    private void Update()
+    void Update()
     {
         if (!IsOwner) return;
 
-        _moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // Flip sprite
+        if (input.x != 0)
+            spriteRenderer.flipX = input.x < 0;
 
         if (Input.GetKeyDown(KeyCode.Space))
-            _fire = true;
-
-        // Flip visual locally for instant feedback
-        if (_moveInput.x != 0)
-            spriteRenderer.flipX = _moveInput.x < 0;
+            fire = true;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         if (!IsOwner) return;
 
-        var input = new InputData
+        Vector2 targetVelocity = input * moveSpeed;
+        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, targetVelocity, moveSmooth * Time.fixedDeltaTime);
+
+        if (input.sqrMagnitude > 0.01f)
         {
-            movement = _moveInput,
-            fire = _fire
-        };
-
-        _fire = false;
-        MoveServerRpc(input);
-    }
-
-    [ServerRpc(RequireOwnership = true)]
-    private void MoveServerRpc(InputData input)
-    {
-        if (!IsServer) return;
-
-        Vector2 targetVelocity = input.movement * movementSpeed;
-        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, targetVelocity, Time.fixedDeltaTime * movementSmoothness);
-
-        if (input.movement != Vector2.zero)
-        {
-            float targetAngle = Mathf.Atan2(-input.movement.x, input.movement.y) * Mathf.Rad2Deg;
-            float smoothAngle = Mathf.LerpAngle(rb.rotation, targetAngle, Time.fixedDeltaTime * rotationSmoothness);
-            rb.rotation = smoothAngle;
+            float targetAngle = Mathf.Atan2(-input.x, input.y) * Mathf.Rad2Deg;
+            float smoothAngle = Mathf.LerpAngle(rb.rotation, targetAngle, rotSmooth * Time.fixedDeltaTime);
+            rb.MoveRotation(smoothAngle);
         }
 
-        if (input.fire)
+        if (fire)
         {
-            Debug.Log($"[Server] Player {OwnerClientId} fired!");
+            fire = false;
+            FireServerRpc();
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    [ServerRpc]
+    void FireServerRpc()
+    {
+        Debug.Log($"[Server] Player {OwnerClientId} used Fire!");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (!IsServer) return;
 
-        if (!collision.gameObject.TryGetComponent(out PlayerMovement otherPlayer))
-            return;
+        if (!collision.gameObject.TryGetComponent(out PlayerMovement other)) return;
 
-        Vector2 direction = (transform.position - otherPlayer.transform.position).normalized;
+        Vector2 direction = (transform.position - other.transform.position).normalized;
         rb.AddForce(direction * bounceForce, ForceMode2D.Impulse);
-    }
-
-    [System.Serializable]
-    public struct InputData : INetworkSerializable
-    {
-        public Vector2 movement;
-        public bool fire;
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
-        {
-            serializer.SerializeValue(ref movement);
-            serializer.SerializeValue(ref fire);
-        }
     }
 }
